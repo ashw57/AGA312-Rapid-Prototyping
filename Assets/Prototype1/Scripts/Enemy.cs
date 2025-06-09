f67387902962968f16b589a664281e870ce90c86
ashw57 <nugoot57@gmail.com>
2025-06-09 18:51:19 +1000

    Prototype 1 Completed

---
+++ Assets/Prototype1/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    void Start() { 
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player"); }
+    void Update() 
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed); 
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+   
+}
---
+++ Assets/Prototype1/Scripts/Powerup.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum PowerUpType { None, Pushback, Smash }
+
+public class Powerup : MonoBehaviour
+{
+    public PowerUpType powerUpType;
+}
