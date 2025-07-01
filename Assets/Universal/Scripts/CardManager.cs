d5d4a93d5153d575a06bd428e9b087985328a1ad
ashw57 <nugoot57@gmail.com>
2025-07-01 17:49:43 +1000

    Wk 5 - Scriptable Objects

---
+++ Assets/Universal/Scripts/CardManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CardManager : MonoBehaviour
+{
+    public List<CardData> cardData;
+    public GameObject cardPrefab;
+    public List<GameObject> cardsInHand;
+    public int handCount = 4;
+
+    public CardData GetCard(CardID _cardID) => cardData.Find(x => x.cardID == _cardID);
+
+    private void BuildDeck()
+    {
+        ListX.DestroyList(cardsInHand);
+        ListX.ShuffleList(cardData);
+
+        for(int i = 0; i < handCount; i++)
+        {
+            GameObject newCard = Instantiate(cardPrefab, new Vector3(i + 2, 0, 0), transform.rotation);
+            newCard.GetComponent<Card>().Initialize(ListX.GetRandomItemFromList(cardData));
+            cardsInHand.Add(newCard);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+            BuildDeck();
+    }
+}
