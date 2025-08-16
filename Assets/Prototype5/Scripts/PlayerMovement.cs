31ad240ff0a01f6174ed6c8400d4b4f7c4b83f86
ashw57 <nugoot57@gmail.com>
2025-08-16 14:51:54 +1000

    Imported assets and updated jump mechanic

--- Assets/Prototype5/Scripts/PlayerMovement.cs
+++ Assets/Prototype5/Scripts/PlayerMovement.cs
@@ -5,20 +5,40 @@ namespace Prototype5
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float speed;
+        [SerializeField] private float jumpHeight = 5f;
+
+        private bool IsGrounded;
         private Rigidbody2D body;
 
         private void Awake()
         {
             body = GetComponent<Rigidbody2D>();
+
         }
 
         private void Update()
         {
             body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
 
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space) && IsGrounded == true)
             {
-                body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+                body.linearVelocity = new Vector2(body.linearVelocity.x, jumpHeight * speed);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                IsGrounded = true;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                IsGrounded = false;
             }
         }
 
